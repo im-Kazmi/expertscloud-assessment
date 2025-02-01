0d461cd482cbc41974ad7cdaf550c7f9c0976b75
im-Kazmi <abidkazmi729@gmail.com>
2025-02-01 17:18:36 +0500

    remove a tiny bug

--- packages/auth/provider.tsx
+++ packages/auth/provider.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
-import type { Theme } from '@clerk/types';
-import { tailwind } from '@repo/tailwind-config';
-import { useTheme } from 'next-themes';
-import type { ComponentProps } from 'react';
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import type { Theme } from "@clerk/types";
+import { tailwind } from "@repo/tailwind-config";
+import { useTheme } from "next-themes";
+import type { ComponentProps } from "react";
 
 export const AuthProvider = (
-  properties: ComponentProps<typeof ClerkProvider>
+  properties: ComponentProps<typeof ClerkProvider>,
 ) => {
   const { resolvedTheme } = useTheme();
-  const isDark = resolvedTheme === 'dark';
+  const isDark = resolvedTheme === "dark";
   const baseTheme = isDark ? dark : undefined;
-  const variables: Theme['variables'] = {
+  const variables: Theme["variables"] = {
     // Core
-    fontFamily: tailwind.theme.fontFamily.sans.join(', '),
-    fontFamilyButtons: tailwind.theme.fontFamily.sans.join(', '),
+    fontFamily: tailwind.theme.fontFamily.sans.join(", "),
+    fontFamilyButtons: tailwind.theme.fontFamily.sans.join(", "),
     fontSize: tailwind.theme.fontSize.sm[0],
     fontWeight: {
       bold: tailwind.theme.fontWeight.bold,
@@ -26,21 +26,21 @@ export const AuthProvider = (
     spacingUnit: tailwind.theme.spacing[4],
   };
 
-  const elements: Theme['elements'] = {
-    dividerLine: 'bg-border',
-    socialButtonsIconButton: 'bg-card',
-    navbarButton: 'text-foreground',
-    organizationSwitcherTrigger__open: 'bg-background',
-    organizationPreviewMainIdentifier: 'text-foreground',
-    organizationSwitcherTriggerIcon: 'text-muted-foreground',
-    organizationPreview__organizationSwitcherTrigger: 'gap-2',
-    organizationPreviewAvatarContainer: 'shrink-0',
+  const elements: Theme["elements"] = {
+    dividerLine: "bg-border",
+    socialButtonsIconButton: "bg-card",
+    navbarButton: "text-foreground",
+    organizationSwitcherTrigger__open: "bg-background",
+    organizationPreviewMainIdentifier: "text-foreground",
+    organizationSwitcherTriggerIcon: "text-muted-foreground",
+    organizationPreview__organizationSwitcherTrigger: "gap-2",
+    organizationPreviewAvatarContainer: "shrink-0",
   };
 
   return (
     <ClerkProvider
-      signInForceRedirectUrl="/onboarding"
-      signUpForceRedirectUrl="/onboarding"
+      signInForceRedirectUrl="/dashboard"
+      signUpForceRedirectUrl="/dashboard"
       {...properties}
       appearance={{ baseTheme, variables, elements }}
     />
